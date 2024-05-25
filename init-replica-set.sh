455286908899998d6e0a43439fac61bad2e05559
davidhei <d4v1dh31@gmail.com>
2024-05-25 21:40:07 +0300

    init-replica-set.sh corrected

--- init-replica-set.sh
+++ init-replica-set.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
-echo "Waiting for MongoDB to start..."
 sleep 10
-
-echo "Initiating the replica set..."
-mongo --host mongo_1:27017 <<EOF
-rs.initiate({
-  _id: "myReplicaSet",
-  members: [
-    { _id: 0, host: "mongo_1:27017" },
-    { _id: 1, host: "mongo_2:27017" },
-    { _id: 2, host: "mongo_3:27017" }
-  ]
-})
-EOF
-
-echo "Replica set initiated."
+echo "Initiating replica set..."
+docker exec -it mongo_1 mongosh --eval "rs.initiate({
+ _id: \"myReplicaSet\",
+ members: [
+   {_id: 0, host: \"mongo_1\"},
+   {_id: 1, host: \"mongo_2\"},
+   {_id: 2, host: \"mongo_3\"}
+ ]
+})"
