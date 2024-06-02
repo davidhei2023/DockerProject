1149e92f1997639678f3e30c06f06d6875a335e1
davidhei2023 <davidhei2010@gmail.com>
2024-06-02 22:52:05 +0300

    Test 45

---
+++ initiate-replica.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Initiating replica set..."
+mongosh --eval "rs.initiate({
+  _id: 'myReplicaSet',
+  members: [
+    { _id: 0, host: 'mongo_1:27017' },
+    { _id: 1, host: 'mongo_2:27017' },
+    { _id: 2, host: 'mongo_3:27017' }
+  ]
+})"
+
+tail -f /dev/null
+
